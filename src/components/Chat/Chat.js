19ac1700cbe9d282db60aa49aace1ddbc35f2d26
taurusilver7 <prasad20697@gmail.com>
2021-02-27 15:10:03 +0530

    chat component added

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
+
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import SendIcon from "@material-ui/icons/Send";
+import MicIcon from "@material-ui/icons/Mic";
+import { useParams } from "react-router-dom";
+import db from "../../firebase";
+import { useStateValue } from "../../context/StateProvider";
+import firebase from "firebase";
+
+const Chat = () => {
+  const [input, setInput] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const { roomId } = useParams();
+  const [messages, setMessages] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snap) => setRoomName(snap.data().name));
+
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snap) => setMessages(snap?.docs.map((doc) => doc.data())));
+    }
+  }, [roomId]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    // console.log('You typed >>>>', input);
+    // Add the messages to firebase database.
+    db.collection("rooms").doc(roomId).collection("messages").add({
+      name: user.displayName,
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+  };
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar
+          src={`https://avatars.dicebear.com/4.5/api/human/${Math.floor(
+            Math.random() * 450
+          )}.svg`}
+        />
+        <div className="chat__headerInfo">
+          <h3>{roomName}</h3>
+          <p>
+            Last seen at{" "}
+            {new Date(
+              messages[messages?.length - 1]?.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${
+              message.name === user?.displayName && "chat__receiver"
+            }`}
+          >
+            <span className="chat__name">{message?.name}</span>
+            {message?.message}
+            <span className="chat__timestamp">
+              {new Date(message.timestamp?.toDate()).toUTCString()}
+            </span>
+          </p>
+        ))}
+      </div>
+
+      <div className="chat__footer">
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            <SendIcon />
+          </button>
+        </form>
+        <IconButton>
+          <MicIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
