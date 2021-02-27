4c03b3eb7e7b9d3e0215bac8d7185a11328b6316
taurusilver7 <prasad20697@gmail.com>
2021-02-27 15:07:44 +0530

    root component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+import Sidebar from "./components/Sidebar/Sidebar";
+import Chat from "./components/Chat/Chat";
+import Login from "./components/Login/Login";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useStateValue } from "./context/StateProvider";
+import { auth } from "./firebase";
+import { actionTypes } from "./context/reducer";
+
+const App = () => {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Switch>
+              <Route exact path="/rooms/:roomId">
+                <Sidebar />
+                <Chat />
+              </Route>
+              <Route exact path="/">
+                <Sidebar />
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
